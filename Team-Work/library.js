02ec0a7c682cf3e46d8e2680c7534c67402565f2
Petko-Petkov <Petko.Hr.Petkov@gmail.com>
2014-07-29 01:25:20 +0300

    Added box object. Updated player image. Moving box left and right works fine. Moving box up and down not so fine. :) TODO: Fix moving objects. (there are errors when more than one box added) TODO: Add end point object.

--- Team-Work/library.js
+++ Team-Work/library.js
@@ -1,13 +1,13 @@
-
-function drawField() {
+/**
+ * Created by PC on 28.7.2014 г..
+ */
+ function drawField() {
     for (var i = 0; i < cH/50; i++) {
-        // drawing horizontal lines
         ctx.beginPath();
-        ctx.moveTo(100 * i, 0);
-        ctx.lineTo(100 * i, cH);
-        // drawing vertical lines
-        ctx.moveTo(0, 100 * i);
-        ctx.lineTo(cW, 100 * i);
+        ctx.moveTo(100*i,0);
+        ctx.lineTo(100*i,cH);
+        ctx.moveTo(0,100*i);
+        ctx.lineTo(cW,100*i);
         ctx.stroke();
         ctx.closePath();
     }
@@ -17,12 +17,36 @@ function drawObjects() {
     for (var row = 0; row < fieldMatrix.length; row++) {
         for (var col = 0; col < fieldMatrix[row].length; col++) {
             switch (fieldMatrix[row][col]) {
-                case 0:
-                    ctx.drawImage(floorImage, col * 100, row * 100, 98, 98);
-                    break;
-                case 1:
-                    ctx.drawImage(bricksImage, col * 100, row * 100, 98, 98);
-                    break;
+                case 0: ctx.drawImage(floorImage, col*100, row*100,98,98); break;
+                case 1: ctx.drawImage(bricksImage, col*100, row*100,98,98); break;
+                case 2: ctx.drawImage(boxImage, col*100, row*100,98, 98); break;
+            }
+        }
+    }
+}
+
+function box (matrix) {
+    this.x = getX(matrix);
+    this.y = getY(matrix);
+    this.w = 98;
+    this.h = 98;
+
+    function getX (matrix) {
+        for (var row = 0; row < matrix.length; row++) {
+            for (var col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 2) {
+                    return row;
+                }
+            }
+        }
+    }
+
+    function getY (matrix) {
+        for (var row = 0; row < matrix.length; row++) {
+            for (var col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 2) {
+                    return col;
+                }
             }
         }
     }
@@ -33,39 +57,32 @@ function Player(x,y) {
     this.y = y;
     this.w = 98;
     this.h = 98;
-//    fieldMatrix[this.y][this.x] = 2;
+    fieldMatrix[this.y][this.x] = 3;
     ctx.fillStyle = "yellow";
 
+
+
     this.draw = function () {
-        if (keyPressed != '') {
-            if (keyPressed == 'left') {
-                ctx.drawImage(boxImage, this.x * 100 + renderCounter, this.y * 100, this.w, this.h);
-            } else if (keyPressed == 'right') {
-                ctx.drawImage(boxImage, this.x * 100 - renderCounter, this.y * 100, this.w, this.h);
-            } else if (keyPressed == 'up') {
-                ctx.drawImage(boxImage, this.x * 100, this.y * 100 + renderCounter, this.w, this.h);
-            } else if (keyPressed == 'down') {
-                ctx.drawImage(boxImage, this.x * 100, this.y * 100-renderCounter, this.w, this.h);
+        if (keyPressed == 'left') {
+            ctx.drawImage(playerImage,this.x*100+renderCounter,this.y*100,this.w,this.h);
+            renderCounter -=2;
+            if (renderCounter == 0) {
+                keyPressed = '';
+                renderCounter = 100;
             }
-            endBoxAnimation();
         } else {
-            ctx.drawImage(boxImage, this.x * 100, this.y * 100, this.w, this.h);
+            ctx.drawImage(playerImage,this.x*100,this.y*100,this.w,this.h);
         }
     };
 }
 
-function endBoxAnimation() {
-    renderCounter -= 4;
-    if (renderCounter == 0) {
-        keyPressed = '';
-        renderCounter = 100;
-    }
+function updatePlayerPosition(x,y) {
+    fieldMatrix[y][x] = 3;
 }
 
-//function updatePlayerPosition(x,y) {
-//    fieldMatrix[y][x] = 2;
-//}
-//function clearPlayerPosition(x,y) {
-//    fieldMatrix[y][x] = 0;
-//}
-
+function updateBoxPosition(x,y) {
+    fieldMatrix[y][x] = 2;
+}
+function clearPlayerPosition(x,y) {
+    fieldMatrix[y][x] = 0;
+}
--- Team-Work/play-field.js
+++ Team-Work/play-field.js
@@ -1,10 +1,14 @@
-
+/**
+ * Created by PC on 28.7.2014 г..
+ */
 var ctx;
 var cW, cH;
 
 var boxImage = new Image(),
+playerImage = new Image(),
 bricksImage = new Image(),
 floorImage = new Image();
+playerImage.src = "images/gamer.png",
 boxImage.src = "images/box.PNG",
 bricksImage.src = "images/bricks.jpg",
 floorImage.src = "images/floor.PNG";
@@ -15,17 +19,19 @@ var renderCounter = 100;
 var fieldMatrix = [
     [1,1,1,1,1,1,1,1],
     [1,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,1],
+    [1,0,2,0,0,0,0,1],
     [1,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1]
 ];
 
 function initCanvas() {
     ctx = document.getElementById("canvas").getContext("2d");
-    cW = ctx.canvas.width, cH = ctx.canvas.height;
+    cW = ctx.canvas.width;
+    cH = ctx.canvas.height;
     var animateInterval;
 
-    var box = new Player(5,1);
+    var gamer = new Player(5,1);
+    var boxx = new box(fieldMatrix);
 
     function animate() {
         ctx.save();
@@ -33,7 +39,7 @@ function initCanvas() {
 
         drawField();
         drawObjects();
-        box.draw();
+        gamer.draw();
 
         ctx.restore();
     }
@@ -42,28 +48,50 @@ function initCanvas() {
 
    document.addEventListener('keydown', function(event) {
 
+       clearPlayerPosition(gamer.x,gamer.y);
        if (event.keyCode == 37) { // left button pressed
-           if (fieldMatrix[box.y][box.x-1] == 0) {
+           if (fieldMatrix[gamer.y][gamer.x-1] == 0) {
                keyPressed = 'left';
-               box.x--;
-           }
+               gamer.x--;
+           } else if ((fieldMatrix[gamer.y][gamer.x-1] == 2) && (fieldMatrix[gamer.y][gamer.x-2] != 1) ) {
+               gamer.x--;
+               boxx.x--; 
+           } 
        } else if (event.keyCode == 39) { // right button pressed
-           if (fieldMatrix[box.y][box.x+1] == 0) {
+           if (fieldMatrix[gamer.y][gamer.x+1] == 0) {
                keyPressed = 'right';
-               box.x++;
-           }
+               gamer.x++;
+           } else if ((fieldMatrix[gamer.y][gamer.x+1] == 2) && (fieldMatrix[gamer.y][gamer.x+2] != 1) ) {
+               gamer.x++;
+               boxx.x++;
+           } 
        } else if (event.keyCode == 38) { // up button pressed
-           if (fieldMatrix[box.y-1][box.x] == 0) {
-               keyPressed = 'up';
-               box.y--;
-           }
-       } else if (fieldMatrix[box.y+1][box.x] == 0) { // down button pressed
-           if (box.y * 100 < cH - 100) {
-               keyPressed = 'down';
-               box.y++;
-           }
+           if (fieldMatrix[gamer.y-1][gamer.x] == 0) {
+               gamer.y--;
+           }else if ((fieldMatrix[gamer.x][gamer.y-1] == 2) && (fieldMatrix[gamer.x][gamer.y-2] != 1) ) {
+               gamer.y--;
+               boxx.y--; 
+           } 
+       } else if (fieldMatrix[gamer.y+1][gamer.x] == 0) { // down button pressed
+           if (gamer.y * 100 < cH - 100) {
+               gamer.y++;
+           } else if ((fieldMatrix[gamer.y+1][gamer.x] == 2) && (fieldMatrix[gamer.y+2][gamer.x] != 1) ) {
+               gamer.y++;
+               boxx.y++; 
+           }            
        }
+       updatePlayerPosition(gamer.x,gamer.y);
+       updateBoxPosition(boxx.x, boxx.y);
    });
+
+    document.addEventListener('keyup', function(event) {
+
+        if (event.keyCode == 37) { // left button released
+                keyPressed = '';
+        } else if (event.keyCode == 39) { // right button released
+                keyPressed = '';
+        }
+    });
 
 }
 
